b19049c403ab165188defc9737c11852ed6b1783
JohnIanOngayi <ongayijohnian@gmail.com>
2024-08-15 16:20:09 +0300

    chore: added function that concatenates two strings

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+Module defines a function concat
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    returns str1 n str2 concatenated
+    """
+    return str1 + str2
