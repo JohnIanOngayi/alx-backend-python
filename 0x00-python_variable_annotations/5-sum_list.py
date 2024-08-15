f814f6f631dcad097e81d6ceedb2fff9d1c0637d
JohnIanOngayi <ongayijohnian@gmail.com>
2024-08-15 16:29:24 +0300

    chore: added function that returns sum of list elements

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""
+Module defines function sum_list
+"""
+
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    returns sum of list elements
+    """
+    return sum(input_list)
