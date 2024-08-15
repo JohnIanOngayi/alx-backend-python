0b9500c1cc3cdf2377b23d22f6e7a8d5fb4d239d
JohnIanOngayi <ongayijohnian@gmail.com>
2024-08-15 16:21:52 +0300

    chore: added function that returns floor of a float

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""
+Module defines function floor
+"""
+
+
+import math
+
+
+def floor(n: float) -> int:
+    """
+    returns the rounded down value of n
+    """
+    return math.floor(n)
