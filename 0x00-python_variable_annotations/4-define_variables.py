648523b4f100d1fcb2bfb77c70ff6cabc41d4dc1
JohnIanOngayi <ongayijohnian@gmail.com>
2024-08-15 16:28:18 +0300

    chore: added four staticly typed variables

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""
+Module defines four type annotated variables
+"""
+
+
+# random int
+a: int = 1
+
+# random float
+pi: float = 3.14
+
+# random bool
+i_understand_annotations: bool = True
+
+# random str
+school: str = "Holberton"
