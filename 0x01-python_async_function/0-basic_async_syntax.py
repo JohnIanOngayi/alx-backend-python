87a8f3a834e749125a0af4c000a4192f189eb8e3
JohnIanOngayi <ongayijohnian@gmail.com>
2024-08-19 12:20:35 +0300

    chore: added an async coroutine that waits for random delay and eventually returns it

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,1 +1,27 @@
 #!/usr/bin/env python3
+
+"""
+Module utilises asyncio to run coroutines
+"""
+
+import time
+import asyncio
+import random
+from typing import Union
+
+
+async def wait_random(max_delay: int = 10) -> Union[float, int]:
+    """
+    Asynchronous coroutine that waits for a random delay between 0 n max_delay
+
+    Args:
+        max_delay (int): The maximum delay in seconds. Default is 10 seconds.
+
+    Returns:
+        Union[float, int]: The actual time waited in seconds.
+    """
+    wait_time: int = random.randint(0, max_delay)
+    start_time: float = time.time()
+    await asyncio.sleep(wait_time)
+    end_time: float = time.time()
+    return end_time - start_time
