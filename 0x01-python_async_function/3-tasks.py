34d81b3ce4338297cce8706c0c88cb8109143676
JohnIanOngayi <ongayijohnian@gmail.com>
2024-08-19 13:02:09 +0300

    chore: added function that returns an asyncIO task

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,1 +1,23 @@
 #!/usr/bin/env python3
+
+"""
+Module defines function that creates an asyncio task
+"""
+
+import asyncio
+from typing import Callable
+wait_random: Callable = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """
+    Creates an asyncio Task that waits for a random delay.
+
+    Args:
+        max_delay (int): The maximum delay in seconds.
+
+    Returns:
+        asyncio.Task: The created asyncio Task.
+    """
+    task: asyncio.Task = asyncio.create_task(wait_random(max_delay))
+    return task
