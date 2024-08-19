5cb0fa41ae46fc06cbef38b8902f8ff276b6a4b0
JohnIanOngayi <ongayijohnian@gmail.com>
2024-08-19 12:50:05 +0300

    chore: added function that waits n times

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,1 +1,28 @@
 #!/usr/bin/env python3
+
+"""
+Module defines function that waits n times with specific max delay time
+"""
+
+import asyncio
+from typing import List, Callable
+wait_random: Callable = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Spawns wait_random n times with the specified max_delay.
+
+    Args:
+        n (int): The number of times to spawn wait_random.
+        max_delay (int): The maximum delay for wait_random.
+
+    Returns:
+        List[float]: A list of elapsed times for each wait_random call,
+                        sorted in ascending order.
+    """
+    tasks = []
+    for _ in range(n):
+        tasks.append(asyncio.create_task(wait_random(max_delay)))
+    elapsed_times = [await task for task in asyncio.as_completed(tasks)]
+    return sorted(elapsed_times)
