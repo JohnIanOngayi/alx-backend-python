5b93ccb94f7b8dffc3648e65976ec72b8051c60e
JohnIanOngayi <ongayijohnian@gmail.com>
2024-08-20 13:02:54 +0300

    chore: added function that yields random numbers

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""
+Module defines coroutine looping 10 times yielding a random no between 1 and 10
+"""
+
+import asyncio
+import random
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """
+    An asynchronous generator that yields a random number between 1 and 10.
+
+    This function will run a loop 10 times, and in each iteration, it will:
+    1. Await for 1 second using asyncio.sleep(1).
+    2. Yield a random floating-point number between 1 and 10.
+
+    Yields:
+        float: A random floating-point number between 1 and 10.
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield (random.uniform(1, 10))
---
+++ 0x02-python_async_comprehension/README.md
@@ -0,0 +1,4 @@
+# 0x02-python_async_comprehension
+
+This repo folder contains code writen as an introduction to async comprehension
+in python
