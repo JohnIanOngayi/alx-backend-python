7c2e959d0ec71da29795815e5413e4b65f384f9b
JohnIanOngayi <ongayijohnian@gmail.com>
2024-09-07 12:20:55 +0300

    task: added unittest method for utils.access_nested_map exceptions

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -26,6 +26,18 @@ class TestAccessNestedMap(unittest.TestCase):
         """Tests the access_nested_map function"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
+    @parameterized.expand(
+        [
+            ({}, ("a",)),
+            ({"a": 1}, ("a", "b")),
+        ]
+    )
+    def test_access_nested_map_exception(
+        self, nested_map: Mapping, path: tuple
+    ) -> None:
+        """Tests the access_nested_map KeyError exception"""
+        self.assertRaises(KeyError, access_nested_map, nested_map, path)
+
 
 #
 # if __name__ == "__main__":
